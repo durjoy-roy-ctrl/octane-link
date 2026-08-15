20b0eff5dd2366e103d81794f1bbb918f8f5c445
Name <durjoyroy735@gmail.com>
2026-08-15 13:42:43 +0600

    Add login and signup pages

--- Octane-Link/src/App.jsx
+++ Octane-Link/src/App.jsx
@@ -1,24 +1,27 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
 
 function App() {
+  const [user, setUser] = useState(null)
 
   function login(name, email) {
+    const loggedInUser = { name, email }
+    setUser(loggedInUser)
     console.log('User:', name)
     console.log('Email:', email)
   }
 
-  return (
-    <div>
-      <Navbar />
+   return (
+    <div className="page">
+      <Navbar user={user} cartCount={0} />
 
       <Routes>
-        <Route
-          path="/signup"
-          element={<Signup login={login} />}
-        />
+        <Route path="/signup" element={<Signup login={login} />} />
+        <Route path="/login" element={<Login login={login} />} />
       </Routes>
 
       <Footer />
