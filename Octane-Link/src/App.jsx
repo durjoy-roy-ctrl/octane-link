2a629c1cca2561c7cfab4efc77e712f5bcedba51
AmdadulHasan-codeeater <amdadhasan678@gmail.com>
2026-08-16 01:35:30 +0600

    added splash screen

--- Octane-Link/src/App.jsx
+++ Octane-Link/src/App.jsx
@@ -1,93 +1,11 @@
-// <<<<<<< HEAD
-// /*import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-// import './App.css'
-// =======
-// import { useState } from 'react'
-// import { Routes, Route } from 'react-router-dom'
-// import Navbar from './components/Navbar.jsx'
-// import Footer from './components/Footer.jsx'
-// import Signup from './pages/Signup.jsx'
-// import Login from './pages/Login.jsx'
-// import Delivery from './pages/Delivery.jsx'
-// import DeliveryTracking from './pages/DeliveryTracking.jsx'
-// import DeliverySchedule from './pages/DeliverySchedule.jsx'
-// >>>>>>> origin/main
 
-// function App() {
-//   const [user, setUser] = useState(null)
-
-//   function login(name, email) {
-//     const loggedInUser = { name, email }
-//     setUser(loggedInUser)
-//     console.log('User:', name)
-//     console.log('Email:', email)
-//   }
-
-//    return (
-//     <div className="page">
-//       <Navbar user={user} cartCount={0} />
-
-//       <Routes>
-//         <Route path="/signup" element={<Signup login={login} />} />
-//         <Route path="/login" element={<Login login={login} />} />
-
-//         {/* Delivery system */}
-//         <Route path="/delivery" element={<Delivery />} />
-//         <Route path="/delivery/track" element={<DeliveryTracking />} />
-//         <Route path="/delivery/schedule" element={<DeliverySchedule />} />
-//       </Routes>
-
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// <<<<<<< HEAD
-// export default App*/
-
-// /*import "./App.css";
-// import ProductCatalog from "./pages/ProductCatalog";
-// function App() {
-//   return <ProductCatalog />;
-// }
-// export default App;*/
-
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import ProductCatalog from "./pages/productCatalog";
-// import ProductDetails from "./pages/productDetails";
-// // import Signup from "./pages/Signup";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<Navigate to="/catalog" replace />} />
-//         <Route path="/catalog" element={<ProductCatalog />} />
-//         <Route path="/product/:id" element={<ProductDetails />}
-// />
-//       </Routes>
-
-//       <Footer />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-// =======
-// export default App
-// >>>>>>> origin/main
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+import SplashScreen from "./components/splashScreen";
 
 import ProductCatalog from "./pages/productCatalog";
 import ProductDetails from "./pages/productDetails";
@@ -100,7 +18,17 @@ import DeliveryTracking from "./pages/DeliveryTracking";
 import DeliverySchedule from "./pages/DeliverySchedule";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   function login(name, email) {
     const loggedInUser = { name, email };
@@ -108,6 +36,10 @@ function App() {
 
     console.log("User:", name);
     console.log("Email:", email);
+  }
+
+  if (showSplash) {
+    return <SplashScreen />;
   }
 
   return (
